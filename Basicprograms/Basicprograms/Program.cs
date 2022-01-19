1f6fabf129e0a28cb1a1d783427670724377b27e
akshata2005 <akshatasawant1805@gmail.com>
2022-01-19 15:06:16 +0530

    [akshata] Adding LeapYear.cs

--- Basicprograms/Basicprograms/Program.cs
+++ Basicprograms/Basicprograms/Program.cs
@@ -10,27 +10,17 @@ namespace Basicprograms
     {
         static void Main(string[] args)
         {
-            int tailCount = 0;
-            int headCount = 0;
-            Console.WriteLine("Please enter number to know how many times to flip the coin");
-            int numbOfTimes = Convert.ToInt32(Console.ReadLine());
-            Random random = new Random();
-
-            for (int i = 1; i <= numbOfTimes; i++)
+            Console.WriteLine("Enter Year : ");
+            int Year = int.Parse(Console.ReadLine());
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
             {
-                double randomValue = random.NextDouble();
-                if (randomValue < 0.5)
-                    tailCount++;
-                else
-                    headCount++;
+                Console.WriteLine("{0} is a Leap Year.", Year);
             }
-            Console.WriteLine("Number of tails is " + tailCount + " out of " + numbOfTimes);
-            Console.WriteLine("Number of heads is " + headCount + " out of " + numbOfTimes);
-
-            double tailPercentage = (double)(tailCount * 100) / numbOfTimes;
-            Console.WriteLine("Tail Percentage:" + tailPercentage);
-            double headPercentage = (double)(headCount * 100) / numbOfTimes;
-            Console.WriteLine("Head Percentage:" + headPercentage);
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year.", Year);
+            }
+            Console.ReadLine();
         }
     }
 }
