e3e4ff7efd7421d0939dd625b9caaf00643b54b0
akshata2005 <akshatasawant1805@gmail.com>
2022-01-20 07:42:17 +0530

    [Akshata] Adding HarmonicSeries.cs

--- Basicprograms/Basicprograms/Program.cs
+++ Basicprograms/Basicprograms/Program.cs
@@ -4,18 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Basicprograms
+namespace HarmonicSeries
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter the number to print power of two");
-            int number = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i <= number; i++)
+            int i, n;
+            double s = 0.0;
+
+            Console.Write("Calculate the harmonic series and their sum\n");
+            Console.Write("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+            for (i = 1; i <= n; i++)
             {
-                Console.WriteLine("power of 2 is" + Math.Pow(2, i));
+                Console.Write("1/{0} + ", i);
+                s += 1 / (float)i;
             }
+            Console.Write("\nSum of Series upto {0} terms is : {1} \n", n, s);
+        }
         }
     }
-}
